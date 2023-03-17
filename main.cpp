23d717d2437496c67277027302fc3bc6841f771b
Ilya <kotyrov.1310@gmail.com>
2023-03-17 21:39:50 +0100

    add example in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#include "Cube.h"
+#include "GeneticSolver.h"
+#include "SequenceParser.h"
+#include <iostream>
+
 int main() {
+    Cube cube;
+    SequenceParser::perform_sequence(cube, "U x B' R2 E U' F E L2");
+
+    GeneticSolver solver(cube);
+    std::cout << solver.solve();
+
     return 0;
 }
