d0043c87d9f8eb9a248c56085974db5e20ccd962
Ilya <kotyrov.1310@gmail.com>
2023-03-17 22:01:49 +0100

    set up OpenGL and other dependencies for graphics

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
-#include "Cube.h"
-#include "GeneticSolver.h"
-#include "SequenceParser.h"
-#include <iostream>
-
 int main() {
-    Cube cube;
-    SequenceParser::perform_sequence(cube, "U x B' R2 E U' F E L2");
-
-    GeneticSolver solver(cube);
-    std::cout << solver.solve();
-
     return 0;
 }
